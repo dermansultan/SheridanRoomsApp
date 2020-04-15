1cd0c0d96b93337ad1ac02e5ec46d9059b60629d
dermansultan <abdurrehmansultan4@gmail.com>
2020-04-15 13:51:27 -0400

    Modal implementation

--- main.js
+++ main.js
@@ -1,7 +1,40 @@
 const rooms = [
     {
-    roomNumber : "219",
-    iMacs: 5,
-    MsftKinect: 1,
-    }
+        roomTitle : "J319",
+        iMacs: 5,
+        MsftKinect: 1,
+        photoStudio: true,
+        Availibility: true,
+        roomDesc: "This is totally one lol",
+    },
+
+    {
+        roomTitle : "J220",
+        iMacs: 5,
+        MsftKinect: 1,
+        Availibility: true,
+        roomDesc: "This is totally one lol",
+    },
+
+    {
+        roomTitle : "J219",
+        iMacs: 3,
+        MsftKinect: 0,
+        Availibility: false,
+        roomDesc: "This is totally one lol",
+    },
+
+    {
+        roomTitle: "J208",
+        iMacs: 5,
+        MsftKinect: 1,
+        Availibility: false,
+        roomDesc: "This is totally one lol",
+    },
+
     ];
+
+    function modal_Fill(clicked_id) {
+//if blah clicked fill modal if blah clicked fill modal with this info if blah clicked fill
+console.log((clicked_id), "was clicked!")
+    };
